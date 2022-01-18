74655b0fe67f52421e74df61bf3bcd7845eca306
Chris Siannas <xrsia@otenet.gr>
2022-01-18 12:09:43 +0200

    Final

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	myquote := `Amateurs practice until they get it right.
 Proffesionals practice until they cant't get it wrong.`
+	// - write quotes.txt to hard disk
+	// - return a file handler for the file
+	fileHandler,_:=os.Create("quotes.txt")
 
-	fmt.Println(myquote)
+	// after exit of main close the file
+	defer fileHandler.Close()
+
+	// we need a belt to convey buffered data
+	myWriterBelt := bufio.NewWriter(fileHandler)
+
+	// our intention is through the belt to 
+	// move our data (myqote) to file
+	fmt.Fprintln(myWriterBelt,myquote)
+
+	// move the data to file
+	myWriterBelt.Flush()
+
+	fmt.Println("Data has been saved to quotes.txt")
 }
