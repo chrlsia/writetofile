bdbb0cac53861c093291672feb6cadd734d7c486
Chris Siannas <xrsia@otenet.gr>
2022-01-18 11:35:50 +0200

    Start

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	myquote := `Amateurs practice until they get it right.
+Proffesionals practice until they cant't get it wrong.`
+
+	fmt.Println(myquote)
+}
